f200a81fd20eeb9310d689f6ce3c768767b4962b
Audrey Nabilla Laksono <audreynabilla006@gmail.com>
2026-03-08 12:09:44 +0700

    Menambahkan halaman dashboard

--- includes/header.php
+++ includes/header.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($page_title) ? $page_title . ' | ' : ''; ?>Sistem Manajemen Inventaris</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/crud/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <div class="container">
-        <!-- Header -->
         <header class="header">
             <div class="header-left">
                 <div class="logo">
