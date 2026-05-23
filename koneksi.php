4359061b5bcca6ebe1419f504017cf880fb0e235
Audrey Nabilla Laksono <audreynabilla006@gmail.com>
2026-05-23 21:10:57 +0700

    Menambahkan folder app

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,14 @@
 <?php
-$host = "localhost";
-$dbname = "crud_db";
-$username = "root";
-$password = "";
+$host = 'localhost';
+$dbname = 'crud_db';
+$user = 'root';
+$pass = '';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    // Mengatur mode error PDO
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $conn;
 } catch (PDOException $e) {
-    echo "Koneksi database gagal: " . $e->getMessage();
+    die('Koneksi database gagal: ' . $e->getMessage());
 }
-?>
